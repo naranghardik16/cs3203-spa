c77f7f5a4f864341586949dfa05c8c6a9fe44fac
Kai Qi <e0556291@u.nus.edu>
2023-01-30 00:00:58 +0800

    Clean up code

--- Team02/Code02/src/spa/src/SP/Token.cpp
+++ Team02/Code02/src/spa/src/SP/Token.cpp
@@ -1,28 +1,5 @@
-//
-// Created by Kai Qi Yong on 29/1/23.
-//
-
 #include "Token.h"
 using namespace std;
-
-/*
-Token::Token(TokenType t, string val) {
-  type_ = t;
-  value_ = val;
-}
-
-TokenType Token::GetType() {
-  return type_;
-}
-
-string Token::GetValue() {
-  return value_;
-}
-
-bool Token::Equals(Token *other) {
-  return this->type_ == other->GetType() && this->value_.compare(other->GetValue()) == 0;
-}
- */
 
 Token::Token(string val) {
   value_ = val;
--- Team02/Code02/src/spa/src/SP/Token.h
+++ Team02/Code02/src/spa/src/SP/Token.h
@@ -1,51 +1,9 @@
-//
-// Created by Kai Qi Yong on 29/1/23.
-//
-//
 #ifndef SPA_SRC_SPA_SRC_TOKEN_H_
 #define SPA_SRC_SPA_SRC_TOKEN_H_
 
 #pragma once
 #include <string>
 using namespace std;
-/*
-enum class TokenType {
-  NAME,
-  INTEGER,
-  LEFT_PARENTHESIS,
-  RIGHT_PARENTHESIS,
-  LEFT_BRACE,
-  RIGHT_BRACE,
-  SEMICOLON,
-  UNDERSCORE,
-  DOUBLE_QUOTE,
-  SINGLE_EQUALS,
-  DOUBLE_EQUALS,
-  NOT,
-  NOT_EQUALS,
-  AND,
-  OR,
-  GT,
-  GTE,
-  LT,
-  LTE,
-  PLUS,
-  MINUS,
-  MULTIPLY,
-  DIV,
-  MOD
-};
-
-class Token {
-  TokenType type_;
-  string value_;
- public:
-  Token(TokenType t, string val);
-  TokenType GetType();
-  string GetValue();
-  bool Equals(Token* other);
-};
-*/
 
 class Token {
   string value_;
