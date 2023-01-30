3f7dbd94f6b41e0e1236349627f84d7e783aafd1
Hardik Narang <naranghardik16@gmail.com>
2023-01-31 04:00:06 +0800

    make rudimentary tables of constant, variable and procedures with create and access functionality

--- Team02/Code02/src/spa/src/PKB.h
+++ Team02/Code02/src/spa/src/PKB.h
@@ -1,21 +1,22 @@
 #pragma once
 
-#include<stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include "PKB/PKBStores.h"
 
 using namespace std;
-typedef short PROC;
 
 class TNode;
 
 class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
-class PKB {
+class PKB: public PKBStores {
 public:
-	static VarTable* varTable; 
-	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
-
+  virtual const VARIABLE_STORE &getVariableStore() const;
+  virtual const PROCEDURE_STORE &getProcedureStore() const;
+  virtual const CONSTANT_STORE &getConstantStore() const;
+  virtual void addConstant(CONSTANT constant);
+  virtual VARIABLE_STORE_INDEX addVariable(VARIABLE variable);
+  virtual PROCEDURE_STORE_INDEX addProcedure(PROCEDURE procedure);
 };
--- Team02/Code02/src/spa/src/PKB/PKBStores.h
+++ Team02/Code02/src/spa/src/PKB/PKBStores.h
@@ -5,4 +5,36 @@
 #include "General/KeyValueStore.h"
 #include "TNode.h"
 
+class PKBStores {
+ public:
+  typedef std::string PROCEDURE;
+  typedef std::string VARIABLE;
+  typedef std::string LINE_NUMBER;
+  typedef std::string CONSTANT;
+  typedef uint64_t VARIABLE_STORE_INDEX;
+  typedef uint64_t PROCEDURE_STORE_INDEX;
+
+  typedef KeyValueStore<VARIABLE, VARIABLE_STORE_INDEX> VARIABLE_STORE;
+  typedef KeyValueStore<PROCEDURE, PROCEDURE_STORE_INDEX> PROCEDURE_STORE;
+  typedef std::unordered_set<CONSTANT> CONSTANT_STORE;
+
+
+  virtual const VARIABLE_STORE &getVariableStore() const = 0;
+
+  virtual const PROCEDURE_STORE &getProcedureStore() const = 0;
+
+  virtual const CONSTANT_STORE &getConstantStore() const = 0;
+
+  virtual void addConstant(CONSTANT constant) = 0;
+
+  virtual VARIABLE_STORE_INDEX addVariable(VARIABLE variable) = 0;
+
+  virtual PROCEDURE_STORE_INDEX addProcedure(PROCEDURE procedure) = 0;
+
+ protected:
+  VARIABLE_STORE variable_store_;
+  PROCEDURE_STORE procedure_store_;
+  CONSTANT_STORE constant_store_;
+
+};
 
