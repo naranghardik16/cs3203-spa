90e5874aee8ebf8b008dbb0dc7db5cb094445d86
Wu HanHui <wuhanhui7@gmail.com>
2023-01-31 14:40:08 +0800

    Add query parser test case

--- Team02/Code02/src/spa/src/QPS/Tokenizer.h
+++ Team02/Code02/src/spa/src/QPS/Tokenizer.h
@@ -10,21 +10,21 @@
 class Tokenizer {
 
  public:
-  static std::unordered_map<std::string, std::vector<std::string>> AddDeclarationsAndStatementsIntoMap(const std::string& query,
+  std::unordered_map<std::string, std::vector<std::string>> AddDeclarationsAndStatementsIntoMap(const std::string& query,
                                                                                                            std::unordered_map<std::string, std::vector<std::string>>& map);
 
-  static std::unordered_map<std::string, std::vector<std::string>> AddSelectSubclausesIntoMap(const std::string& clause, std::unordered_map<std::string, std::vector<std::string>>& map);
+  std::unordered_map<std::string, std::vector<std::string>> AddSelectSubclausesIntoMap(const std::string& clause, std::unordered_map<std::string, std::vector<std::string>>& map);
 
 
-  static std::unordered_map<std::string, std::vector<std::string>> AddSubclausesIntoMap(const std::string& clause,
+  std::unordered_map<std::string, std::vector<std::string>> AddSubclausesIntoMap(const std::string& clause,
                                                                                         std::unordered_map<std::string, std::vector<std::string>>& map);
 
-  static std::unordered_map<std::string, std::vector<std::string>> AddSynonymIntoMap(const std::string& clause,
+  std::unordered_map<std::string, std::vector<std::string>> AddSynonymIntoMap(const std::string& clause,
                                                                                          std::unordered_map<std::string, std::vector<std::string>>& map);
 
-  static std::unordered_map<std::string, std::vector<std::string>> TokenizeQuery(const std::string& query);
+  std::unordered_map<std::string, std::vector<std::string>> TokenizeQuery(const std::string& query);
 
-  static std::vector<size_t> GetIndexListOfClauses(const std::string& statement);
+  std::vector<size_t> GetIndexListOfClauses(const std::string& statement);
 
-  static size_t FindStartOfSubClauseIndex(const std::string& s, const std::regex& rgx);
+  size_t FindStartOfSubClauseIndex(const std::string& s, const std::regex& rgx);
 };
