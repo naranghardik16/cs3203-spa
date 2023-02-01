6b7125174f721865350993272bc6ec60403a2327
flairekq <31890138+flairekq@users.noreply.github.com>
2023-02-02 00:04:52 +0800

    Remove redundant code

--- Team02/Code02/src/spa/src/General/Utilities.h
+++ Team02/Code02/src/spa/src/General/Utilities.h
@@ -1,10 +1,6 @@
-#ifndef SPA_SRC_SPA_SRC_GENERAL_UTILITIES_H_
-#define SPA_SRC_SPA_SRC_GENERAL_UTILITIES_H_
 #pragma once
 
 namespace utilities {
   template<typename Base, typename T>
   bool InstanceOf(const T *ptr);
 }
-
-#endif //SPA_SRC_SPA_SRC_GENERAL_UTILITIES_H_
