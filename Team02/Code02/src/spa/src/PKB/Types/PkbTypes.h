c27a7d651249f58e121d57ea1d82000669cc0b28
s7manth <syalamarty002@gmail.com>
2023-02-05 19:04:14 +0800

    replace individual index types with a single one

--- Team02/Code02/src/spa/src/PKB/Types/PkbTypes.h
+++ Team02/Code02/src/spa/src/PKB/Types/PkbTypes.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <string>
+#include <unordered_set>
+#include <variant>
 
 class PkbTypes {
  public:
   typedef std::string PROCEDURE;
   typedef std::string VARIABLE;
-  typedef std::string LINE_NUMBER;
   typedef std::string CONSTANT;
-  typedef uint64_t VARIABLE_STORE_INDEX;
-  typedef uint64_t PROCEDURE_STORE_INDEX;
+  typedef std::size_t LINE_NUMBER;
+
+  typedef std::unordered_set<VARIABLE> VARIABLE_SET;
+  typedef std::unordered_set<LINE_NUMBER> LINE_SET;
+  typedef std::variant<VARIABLE_SET, PROCEDURE> MODIFIES;
+
+  typedef uint64_t INDEX;
 };
