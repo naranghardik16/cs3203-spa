c60d3493783eea32587ad1ddaf2e050275bd4f7e
Sourabh Jaiswal <sourabhrj31@gmail.com>
2023-02-06 20:11:45 +0800

    ✨ add: AbstractionExtractor for modifies

--- Team02/Code02/src/spa/src/SP/DesignExtractor/AbstractionExtractor.cpp
+++ Team02/Code02/src/spa/src/SP/DesignExtractor/AbstractionExtractor.cpp
@@ -1,2 +1,36 @@
 #pragma once
 #include "AbstractionExtractor.h"
+
+AbstractionExtractor::AbstractionExtractor(PKB *pkb) : pkb_(pkb) {
+  pkb_write_facade_ = new PkbWriteFacade(*pkb);
+}
+
+void AbstractionExtractor::VisitAssignStatement(AssignStatement *assign_statement) {
+  PkbTypes::STATEMENT_NUMBER
+      stmt_number = std::to_string(assign_statement->GetStatementNumber());
+  pkb_write_facade_->AddStatementModifyingVariable(stmt_number,
+                                                   assign_statement->GetVariable().GetName());
+}
+
+void AbstractionExtractor::VisitPrintStatement(PrintStatement *print_statement) {
+  // TODO: Abstractions other than Modifies
+}
+
+void AbstractionExtractor::VisitReadStatement(ReadStatement *read_statement) {
+  PkbTypes::STATEMENT_NUMBER
+      stmt_number = std::to_string(read_statement->GetStatementNumber());
+  pkb_write_facade_->AddStatementModifyingVariable(stmt_number,
+                                                   read_statement->GetVariable().GetName());
+}
+
+void AbstractionExtractor::VisitProcedure(Procedure *procedure) {
+  // TODO:
+}
+
+void AbstractionExtractor::VisitConstant(Constant *constant) {
+  // TODO:
+}
+
+void AbstractionExtractor::VisitVariable(Variable *variable) {
+  // TODO:
+}
--- Team02/Code02/src/spa/src/SP/DesignExtractor/AbstractionExtractor.h
+++ Team02/Code02/src/spa/src/SP/DesignExtractor/AbstractionExtractor.h
@@ -1,6 +1,26 @@
 #pragma once
 #include "ParserVisitor.h"
+#include "./core/model/Procedure.h"
+#include "core/model/Constant.h"
+#include "core/model/Variable.h"
+#include "core/model/AssignStatement.h"
+#include "core/model/PrintStatement.h"
+#include "core/model/ReadStatement.h"
 
 class AbstractionExtractor : public ParserVisitor {
-
+ public:
+  AbstractionExtractor(PKB *pkb);
+  void VisitAssignStatement(AssignStatement *assign_statement) override;
+  void VisitPrintStatement(PrintStatement *print_statement) override;
+  void VisitReadStatement(ReadStatement *read_statement) override;
+  void VisitProgram(Program *program);
+  void VisitProcedure(Procedure *procedure) override;
+  void VisitVariable(Variable *variable) override;
+  void VisitConstant(Constant *constant) override;
+  void VisitStatement(Statement *statement);
+  void VisitExpression(Expression *expression);
+ private:
+  //TODO: Design Extractor should have one pkb and write_face
+  PKB *pkb_;
+  PkbWriteFacade *pkb_write_facade_;
 };
