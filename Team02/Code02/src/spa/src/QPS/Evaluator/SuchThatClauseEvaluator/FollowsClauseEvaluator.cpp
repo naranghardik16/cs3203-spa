02ac439f5b82406186a6dacd96a6caf33db684a9
isabelteo <83572953+isabelteo@users.noreply.github.com>
2023-02-07 16:31:00 +0800

    Fix logic in ClauseEvaluator

--- Team02/Code02/src/spa/src/QPS/Evaluator/SuchThatClauseEvaluator/FollowsClauseEvaluator.cpp
+++ Team02/Code02/src/spa/src/QPS/Evaluator/SuchThatClauseEvaluator/FollowsClauseEvaluator.cpp
@@ -7,90 +7,86 @@
 bool FollowsClauseEvaluator::EvaluateBooleanConstraint(std::shared_ptr<PkbReadFacade> pkb) {
   auto declaration_map = ClauseEvaluator::GetDeclarationMap();
 
-  bool is_first_arg_a_type_of_statement_synonym = QueryUtil::IsATypeOfStatementSynonym(declaration_map, first_arg_);
   bool is_first_arg_an_integer = LexicalRuleValidator::IsInteger(first_arg_);
   bool is_first_arg_a_wildcard = QueryUtil::IsWildcard(first_arg_);
 
   bool is_second_arg_a_wildcard = QueryUtil::IsWildcard(second_arg_);
   bool is_second_arg_a_integer = LexicalRuleValidator::IsInteger(second_arg_);
-  bool is_second_arg_a_type_of_statement_synonym = QueryUtil::IsATypeOfStatementSynonym(declaration_map, second_arg_);
 
-  std::vector<std::vector<std::string>> result;
-  const std::string kEmpty = "NOT_TYPE_CONSTRAINT";
+  ResultTable table;
+  const std::string kEmpty = "_";
 
   if (is_first_arg_a_wildcard) {
     if (is_second_arg_a_wildcard) {
       //e.g. Follows(_,_) -- return all Follows relationships between statements
-      result = pkb->GetFollowPairs(kEmpty, kEmpty);
-    } else if (is_second_arg_a_type_of_statement_synonym) {
-      //e.g. Follows(_, s) --> Get statements that follow another statement
-      auto constraints = pkb->GetFollowPairs(declaration_map[second_arg_], kEmpty);
-      result = QueryUtil::ExtractSecondElementInTheVectors(constraints);
-    } else {
+      table = pkb->GetFollowPairs(kEmpty, kEmpty);
+    }
+    if (is_second_arg_a_integer) {
       //e.g. Follows(_,"5") --> Get all types of statements that "5" follows
-      result = pkb->GetStatementsFollowedBy(second_arg_, kEmpty);
+      table = pkb->GetStatementsFollowedBy(second_arg_, kEmpty);
     }
   }
+
+  if (is_first_arg_an_integer) {
+    if (is_second_arg_a_wildcard) {
+      //e.g. Follows("5", _) --> Get all types of statements that follow "5"
+      table = pkb->GetStatementsFollowing(first_arg_, kEmpty);
+    }
+    if (is_second_arg_a_integer) {
+      //e.g. Follows(5, 6)
+      return pkb->IsFollows(first_arg_, second_arg_);
+    }
+  }
+
+  return !table.empty();
+
+}
+
+
+std::shared_ptr<Result> FollowsClauseEvaluator::EvaluateClause(std::shared_ptr<PkbReadFacade> pkb) {
+  auto syntax_pair = ClauseEvaluator::GetSyntaxPair();
+  auto declaration_map = ClauseEvaluator::GetDeclarationMap();
+
+
+  bool is_first_arg_a_type_of_statement_synonym = QueryUtil::IsATypeOfStatementSynonym(declaration_map, first_arg_);
+  bool is_first_arg_an_integer = LexicalRuleValidator::IsInteger(first_arg_);
+  bool is_first_arg_a_wildcard = QueryUtil::IsWildcard(first_arg_);
+
+  bool is_second_arg_a_type_of_statement_synonym = QueryUtil::IsATypeOfStatementSynonym(declaration_map, second_arg_);
+  bool is_second_arg_a_wildcard = QueryUtil::IsWildcard(second_arg_);
+
+  ResultTable table;
+  const std::string kEmpty = "_";
 
   if (is_first_arg_a_type_of_statement_synonym) {
     if (is_second_arg_a_wildcard) {
       //e.g. Follows(s, _) --> Get statements that have followers
       auto constraints = pkb->GetFollowPairs(declaration_map[first_arg_], kEmpty);
-      result = QueryUtil::ExtractFirstElementInTheVectors(constraints);
+      table = QueryUtil::ExtractFirstElementInTheVectors(constraints);
     } else if (is_second_arg_a_type_of_statement_synonym) {
       //e.g. Follows(a,p)
-      auto constraints = pkb->GetFollowPairs(declaration_map[first_arg_], declaration_map[second_arg_]);
+      table = pkb->GetFollowPairs(declaration_map[first_arg_], declaration_map[second_arg_]);
     } else {
       //e.g. Follows(a,"5") --> Get statement that 5 follows of type assignment
-      result = pkb->GetStatementsFollowedBy(second_arg_, declaration_map[first_arg_]);
+      table = pkb->GetStatementsFollowedBy(second_arg_, declaration_map[first_arg_]);
     }
   }
 
-    if (is_first_arg_an_integer) {
-      if (is_second_arg_a_wildcard) {
-        //e.g. Follows("5", _) --> Get all types of statements that follow "5"
-        result = pkb->GetStatementsFollowing(first_arg_, kEmpty);
-      } else if (is_second_arg_a_type_of_statement_synonym) {
-        //e.g. Follows("5", a) --> Get statement that follow 5 of type assignment
-        result = pkb->GetStatementsFollowing(first_arg_, declaration_map[second_arg_]);
-      } else {
-        //e.g. Follows(5, 6)
-        return pkb->IsFollows(first_arg_, second_arg_);
-      }
-    }
-
-    return !result.empty();
+  //Second arg must be a synonym by rule of deciding non-boolean constraints
+  if (is_first_arg_an_integer) {
+    //e.g. Follows("5", a) --> Get statement that follow 5 of type assignment
+    table = pkb->GetStatementsFollowing(first_arg_, declaration_map[second_arg_]);
   }
 
-
-std::shared_ptr<Result> FollowsClauseEvaluator::EvaluateClause(std::shared_ptr<PkbReadFacade> pkb) {
-    auto syntax_pair = ClauseEvaluator::GetSyntaxPair();
-    auto declaration_map = ClauseEvaluator::GetDeclarationMap();
-
-    bool is_first_arg_a_type_of_statement_synonym = QueryUtil::IsATypeOfStatementSynonym(declaration_map, first_arg_);
-    bool is_first_arg_an_integer = LexicalRuleValidator::IsInteger(first_arg_);
-    bool is_second_arg_a_type_of_statement_synonym = QueryUtil::IsATypeOfStatementSynonym(declaration_map, second_arg_);
-
-    std::vector<std::vector<std::string>> result;
-
-    if (is_first_arg_a_type_of_statement_synonym) {
-      if (is_second_arg_a_type_of_statement_synonym) {
-        //e.g. Follows(a,p)
-        result = pkb->GetFollowPairs(declaration_map[first_arg_], declaration_map[second_arg_]);
-      } else {
-        //e.g. Follows(a,"5") --> Get statement that 5 follows of type assignment
-        result = pkb->GetStatementsFollowedBy(second_arg_, declaration_map[first_arg_]);
-      }
-    }
-
-    if (is_first_arg_an_integer && is_second_arg_a_type_of_statement_synonym) {
-      //e.g. Follows("5", a) --> Get statement that follow 5 of type assignment
-      result = pkb->GetStatementsFollowing(first_arg_, declaration_map[second_arg_]);
-    }
-
-    ResultHeader header;
-    ResultTable table;
-    std::shared_ptr<Result> result_ptr = std::make_shared<Result>(header, table);
-    return result_ptr;
+  //Second arg must be a synonym by rule of non-boolean constraints
+  if (is_first_arg_a_wildcard) {
+    //e.g. Follows(_, s) --> Get statements that follow another statement
+    auto constraints = pkb->GetFollowPairs(declaration_map[second_arg_], kEmpty);
+    table = QueryUtil::ExtractSecondElementInTheVectors(constraints);
   }
+
+  ResultHeader header;
+  std::shared_ptr<Result> result_ptr = std::make_shared<Result>(header, table);
+  return result_ptr;
+}
 
--- Team02/Code02/src/spa/src/QPS/Evaluator/SuchThatClauseEvaluator/ModifiesClauseEvaluator.cpp
+++ Team02/Code02/src/spa/src/QPS/Evaluator/SuchThatClauseEvaluator/ModifiesClauseEvaluator.cpp
@@ -6,45 +6,23 @@
 bool ModifiesClauseEvaluator::EvaluateBooleanConstraint(std::shared_ptr<PkbReadFacade> pkb) {
   auto declaration_map = ClauseEvaluator::GetDeclarationMap();
 
-  bool is_first_arg_a_type_of_statement_synonym = QueryUtil::IsAssignSynonym(declaration_map, first_arg_)
-      || QueryUtil::IsReadSynonym(declaration_map, first_arg_) || QueryUtil::IsContainerStatementSynonym(declaration_map, first_arg_);
   bool is_first_arg_an_integer = LexicalRuleValidator::IsInteger(first_arg_);
-
   bool is_second_arg_a_wildcard = QueryUtil::IsWildcard(second_arg_);
   bool is_second_arg_a_variable_synonym = QueryUtil::IsVariableSynonym(declaration_map, second_arg_);
-  bool is_second_arg_a_ident = QueryUtil::IsQuoted(second_arg_);
 
-  ResultTable table;
-
-  if (is_first_arg_a_type_of_statement_synonym) {
-    if (is_second_arg_a_wildcard) {
-      //e.g. Select s such that Modifies(s, _) --> is there a statement that modifies a variable?
-      //IsAStatementThatModifiesAVariable(s, stmttype)
-      auto constraints = pkb->GetModifiesStatementVariablePairs(declaration_map[first_arg_]);
-      table = QueryUtil::ExtractFirstElementInTheVectors(constraints);
-    } else if (is_second_arg_a_variable_synonym) {
-      //e.g. Select a1 such that Modifies(a,v) -- is there an assignment that modifies a variable?
-      return true;
-    } else {
-      //e.g. Select a1 such that Modifies(a,”count”) -- is there an assignment that modifies "count"
-      table = pkb->GetStatementsModifiesVariable(second_arg_, declaration_map[first_arg_]);
-    }
-  }
-
+  //! If first arg is wildcard then semantic error; If first arg is synonym then it is not considered boolean constraint
   if (is_first_arg_an_integer) {
     if (is_second_arg_a_wildcard) {
       //Modifies(5, _) -- does 5 modify any variables?
-      return true;
+      return !pkb->GetVariablesModifiedByStatement(first_arg_).empty();
     } else if (is_second_arg_a_variable_synonym) {
       //Select v1 such that Modifies(5,v) -- does 5 modify any variables?
-      return true;
+      return !pkb->GetVariablesModifiedByStatement(first_arg_).empty();
     } else {
       //Modifies(5, "count") -- does 5 modify "count"?
       return pkb->IsModifiesStatement(first_arg_, second_arg_);
     }
   }
-
-  return !table.empty();
 }
 
 std::shared_ptr<Result> ModifiesClauseEvaluator::EvaluateClause(std::shared_ptr<PkbReadFacade> pkb) {
@@ -61,8 +39,12 @@ std::shared_ptr<Result> ModifiesClauseEvaluator::EvaluateClause(std::shared_ptr<
 
   if (is_first_arg_a_type_of_statement_synonym) {
     auto type = declaration_map[first_arg_];
-    //e.g. Modifies(a,v)
-    if (is_second_arg_a_variable_synonym) {
+    if (is_second_arg_a_wildcard) {
+      //e.g. Select s such that Modifies(s, _) --> is there a statement that modifies a variable?
+      auto constraints = pkb->GetModifiesStatementVariablePairs(declaration_map[first_arg_]);
+      table = QueryUtil::ExtractFirstElementInTheVectors(constraints);
+    } else if (is_second_arg_a_variable_synonym) {
+      //e.g. Modifies(a,v)
       table = pkb->GetModifiesStatementVariablePairs(type);
     } else {
       //e.g. Modifies(a,”count”) -- get assignments that modify count
