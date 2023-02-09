8b14e4f42a2277fb16621cbfdba10caa3e8d1351
isabelteo <83572953+isabelteo@users.noreply.github.com>
2023-02-10 01:11:59 +0800

    Refactor Modifies to ModifiesS and ModifiesP

--- Team02/Code02/src/spa/src/QPS/Evaluator/SuchThatClauseEvaluator/ModifiesSClauseEvaluator.cpp
+++ Team02/Code02/src/spa/src/QPS/Evaluator/SuchThatClauseEvaluator/ModifiesSClauseEvaluator.cpp
@@ -1,53 +1,39 @@
 #pragma once
-#include "ModifiesClauseEvaluator.h"
+#include "ModifiesSClauseEvaluator.h"
 #include "General/LexicalRuleValidator.h"
 #include "QPS/Util/QueryUtil.h"
 #include "PKB/Types/PkbCommunicationTypes.h"
 
-bool ModifiesClauseEvaluator::EvaluateBooleanConstraint(std::shared_ptr<PkbReadFacade> pkb) {
+bool ModifiesSClauseEvaluator::EvaluateBooleanConstraint(std::shared_ptr<PkbReadFacade> pkb) {
   auto declaration_map = ClauseEvaluator::GetDeclarationMap();
-  bool is_first_arg_an_integer = LexicalRuleValidator::IsInteger(first_arg_);
   bool is_second_arg_a_wildcard = QueryUtil::IsWildcard(second_arg_);
 
   //! first_arg is stmtref: If first arg is wildcard then semantic error; If first arg is synonym then it is not considered boolean constraint
-  //! So first arg is either ident or int
+  //! So first arg must be int
   //! Based on rule of boolean constraint, the 2nd arg cannot be a synonym so either wildcard or ident (since 2nd arg is entref)
 
-  if (is_first_arg_an_integer) {
-    if (is_second_arg_a_wildcard) {
+  if (is_second_arg_a_wildcard) {
       //Modifies(5, _) -- does 5 modify any variables?
       return !pkb->GetVariablesModifiedByStatement(first_arg_).empty();
-    } else {
-      //Modifies(5, "count") -- does 5 modify "count"?
-      return pkb->HasModifiesStatementRelationship(first_arg_, second_arg_);
-    }
   } else {
-    if (is_second_arg_a_wildcard) {
-      //Modifies("anya", _) -- does the procedure anya modify any variables?
-      return !pkb->GetVariablesModifiedByProcedure(first_arg_).empty();
-    } else {
-      //Modifies("anya", "count") -- does the procedure anya modify "count"?
-      return pkb->HasModifiesProcedureRelationship(first_arg_, second_arg_);
-    }
+    //Modifies(5, "count") -- does 5 modify "count"?
+    return pkb->HasModifiesStatementRelationship(first_arg_, second_arg_);
   }
 }
 
-std::shared_ptr<Result> ModifiesClauseEvaluator::EvaluateClause(std::shared_ptr<PkbReadFacade> pkb) {
+std::shared_ptr<Result> ModifiesSClauseEvaluator::EvaluateClause(std::shared_ptr<PkbReadFacade> pkb) {
   auto declaration_map = ClauseEvaluator::GetDeclarationMap();
 
   //!Validated in validator as a statement synonym or its subset
   bool is_first_arg_a_type_of_statement_synonym = QueryUtil::IsATypeOfStatementSynonym(declaration_map, first_arg_);
   bool is_first_arg_an_integer = LexicalRuleValidator::IsInteger(first_arg_);
 
-  bool is_first_arg_a_procedure_synonym = QueryUtil::IsProcedureSynonym(declaration_map, first_arg_);
-  bool is_first_arg_an_ident = QueryUtil::IsQuoted(first_arg_);
-
   //!Validated in validator as a variable synonym
   bool is_second_arg_a_variable_synonym = declaration_map.count(second_arg_);
   bool is_second_arg_a_wildcard = QueryUtil::IsWildcard(second_arg_);
 
   ResultHeader header;
-  if (is_first_arg_a_type_of_statement_synonym || is_first_arg_a_procedure_synonym) {
+  if (is_first_arg_a_type_of_statement_synonym) {
     header.push_back(first_arg_);
   }
   if (is_second_arg_a_variable_synonym) {
@@ -76,26 +62,6 @@ std::shared_ptr<Result> ModifiesClauseEvaluator::EvaluateClause(std::shared_ptr<
   if (is_first_arg_an_integer) {
     //e.g.Select v such that Modifies(5,v)
     single_constraint = pkb->GetVariablesModifiedByStatement(first_arg_);
-  }
-
-  //! Modifies Procedure
-  if (is_first_arg_a_procedure_synonym) {
-    if (is_second_arg_a_wildcard) {
-      //e.g. Select p such that Modifies(p, _) / Select p1 such that Modifies(p1,_)
-      // Should return the procedure that have a Modifies relationship with any variable
-      single_constraint = pkb->GetProceduresThatModify();
-    } else if (is_second_arg_a_variable_synonym) {
-      //e.g. Modifies(p,v)
-      pair_constraint = pkb->GetModifiesProcedureVariablePairs();
-    } else {
-      //e.g. Modifies(p,”count”) -- get procedures that modify count
-      single_constraint = pkb->GetProceduresModifiesVariable(second_arg_);
-    }
-  }
-
-  if (is_first_arg_an_ident) {
-    //e.g.Select v such that Modifies("anya",v)
-    single_constraint = pkb->GetVariablesModifiedByProcedure(first_arg_);
   }
 
   ResultTable table;
