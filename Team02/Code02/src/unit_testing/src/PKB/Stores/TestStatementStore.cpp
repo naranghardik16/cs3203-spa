f55a81c63d5aa52234d48f0b68daa44a598f3ba9
s7manth <syalamarty002@gmail.com>
2023-02-13 18:03:24 +0800

    add framework for statement store testing

--- Team02/Code02/src/unit_testing/src/PKB/Stores/TestStatementStore.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestStatementStore.cpp
@@ -1,9 +1,10 @@
 #include <catch.hpp>
 
 #include "PKB/Stores/StatementStore.h"
+#include "General/StatementTypeEnum.h"
 
 TEST_CASE("Testcases for Modifies Store") {
-  SECTION("Basic Statement Number to Variable Mapping") {
+  SECTION("Basic Statement Type to Number Mapping") {
     auto statement_store = new StatementStore();
   }
 }
