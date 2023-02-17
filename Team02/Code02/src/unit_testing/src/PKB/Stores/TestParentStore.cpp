6a336ef47f874708f9c9d8cdebf79d5fc6e88808
s7manth <syalamarty002@gmail.com>
2023-02-17 16:12:39 +0800

    add fix for storing transitive uses and modifes relation between statements of different hierarchy

--- Team02/Code02/src/unit_testing/src/PKB/Stores/TestParentStore.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestParentStore.cpp
@@ -1,16 +1,25 @@
 #include <catch.hpp>
-
 #include "PKB/Stores/ParentStore.h"
 
 TEST_CASE("Testcases for Parent Store") {
-  SECTION("Basic Statement Number to Variable Mapping") {
-    auto parent_store = new ParentStore();
+  SECTION("Basic Mapping") {
+    auto* parent_store = new ParentStore();
 
     parent_store->addParentRelation("1", "2");
-    parent_store->addParentRelation("1", "3");
-//    parent_store->addParentRelation("2", "4");
+    parent_store->addParentRelation("1", "4");
+    parent_store->addParentRelation("1", "5");
+    parent_store->addParentRelation("2", "3");
 
-    REQUIRE(parent_store->retrieveAllParents() == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({ "1" }));
+    REQUIRE(parent_store->retrieveAllParentPairs() ==
+    std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER, PkbTypes::STATEMENT_NUMBER>,
+        PairHasherUtil::hash_pair>({ std::make_pair("1", "2"), std::make_pair("1", "4"),
+                                     std::make_pair("1", "5"), std::make_pair("2", "3") }));
+
+    REQUIRE(parent_store->retrieveAllParentStarPairs() ==
+        std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER, PkbTypes::STATEMENT_NUMBER>,
+                           PairHasherUtil::hash_pair>({ std::make_pair("1", "2"), std::make_pair("1", "4"),
+                                                        std::make_pair("1", "5"), std::make_pair("2", "3"),
+                                                        std::make_pair("1", "3") }));
 
   }
 }
