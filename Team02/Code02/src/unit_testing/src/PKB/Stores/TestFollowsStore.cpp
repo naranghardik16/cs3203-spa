68a794ec5831330ccb251fda73b694c2118b9147
s7manth <syalamarty002@gmail.com>
2023-02-17 21:42:55 +0800

    resolve rebase for follows store testing

--- Team02/Code02/src/unit_testing/src/PKB/Stores/TestFollowsStore.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestFollowsStore.cpp
@@ -1,15 +1,24 @@
-//#include <catch.hpp>
-//
-//#include "PKB/Stores/ModifiesStore.h"
-//
-//TEST_CASE("Testcases for Modifies Store") {
-//  SECTION("Basic Statement Number to Variable Mapping") {
-//    ModifiesStore* modifies_store = new ModifiesStore();
-//
-//    modifies_store->addStatementModifyingVariable("2", "a");
-//    modifies_store->addStatementModifyingVariable("2", "b");
-//
-//    REQUIRE(modifies_store->retrieveAllVariablesModifiedByAStatement("2") == std::unordered_set<std::string>({ "a", "b" }));
-//  }
-//}
-//
+#include <catch.hpp>
+#include "PKB/Stores/FollowsStore.h"
+
+TEST_CASE("Testcases for Follows Store") {
+  SECTION("Basic Statement Number to Variable Mapping") {
+    FollowsStore* follows_store = new FollowsStore();
+
+    follows_store->addFollowsRelation("1", "2");
+    follows_store->addFollowsRelation("2", "3");
+    follows_store->addFollowsRelation("3", "4");
+
+    REQUIRE(follows_store->retrieveAllFollowsPairs() ==
+    std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER, PkbTypes::STATEMENT_NUMBER>,
+        PairHasherUtil::hash_pair>({ std::make_pair("1", "2"), std::make_pair("2", "3"),
+                                     std::make_pair("3", "4") }));
+
+    REQUIRE(follows_store->retrieveAllFollowsStarPairs() ==
+        std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER, PkbTypes::STATEMENT_NUMBER>,
+            PairHasherUtil::hash_pair>({ std::make_pair("1", "2"), std::make_pair("2", "3"),
+                                         std::make_pair("1", "3"), std::make_pair("3", "4"),
+                                         std::make_pair("2", "4"), std::make_pair("1", "4") }));
+
+  }
+}
