4da5663289f5e0babc5e38e32672b7ea52f418ad
s7manth <syalamarty002@gmail.com>
2023-02-17 17:50:52 +0800

    add fixes to the single clause modifies s queries

--- Team02/Code02/src/unit_testing/src/PKB/Interfaces/TestPkbReadFacade.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Interfaces/TestPkbReadFacade.cpp
@@ -12,22 +12,26 @@ TEST_CASE("Testing PkbReadFacade") {
     pkb_read_facade_ = new PkbReadFacade(pkb_);
     pkb_write_facade_ = new PkbWriteFacade(pkb_);
 
+    pkb_write_facade_->AddStatementOfAType("1", READ);
     pkb_write_facade_->AddStatementOfAType("2", IF);
     pkb_write_facade_->AddStatementOfAType("3", ASSIGN);
     pkb_write_facade_->AddStatementOfAType("4", ASSIGN);
 
-
     pkb_write_facade_->AddParentRelation("2", "3");
     pkb_write_facade_->AddParentRelation("2", "4");
 
+    pkb_write_facade_->AddStatementModifyingVariable("1", "x");
     pkb_write_facade_->AddStatementModifyingVariable("3", "z");
+    pkb_write_facade_->AddStatementModifyingVariable("4", "x");
 
 
     REQUIRE(pkb_read_facade_->GetVariablesModifiedByStatement("3") == std::unordered_set<std::string>({ "z" }));
-    REQUIRE(pkb_read_facade_->GetVariablesModifiedByStatement("2") == std::unordered_set<std::string>({ "z" }));
+    REQUIRE(pkb_read_facade_->GetVariablesModifiedByStatement("2") == std::unordered_set<std::string>({ "z", "x" }));
 
-    REQUIRE(pkb_read_facade_->GetStatements() == std::unordered_set<std::string>({ "4", "2", "3" }));
-    REQUIRE(pkb_read_facade_->GetStatementsThatModify(STATEMENT) == std::unordered_set<std::string>({ "2", "3" }));
+    REQUIRE(pkb_read_facade_->GetStatements() == std::unordered_set<std::string>({ "4", "2", "3", "1" }));
+    REQUIRE(pkb_read_facade_->GetStatementsThatModify(STATEMENT) == std::unordered_set<std::string>({ "2", "3", "4", "1" }));
+
+    REQUIRE(pkb_read_facade_->GetStatementsModifiesVariable("z", STATEMENT) == std::unordered_set<std::string>({ "2", "3" }));
   }
 
 }
