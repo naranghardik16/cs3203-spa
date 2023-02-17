df8010c5565b122221c25fee89dd4e1ce585f85b
Hardik Narang <naranghardik16@gmail.com>
2023-02-17 14:17:48 +0800

    :white_check_mark: add unit test cases for empty modifies store and single statement modifying a single variable in modifies store

--- Team02/Code02/src/unit_testing/src/PKB/Stores/TestModifiesStore.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestModifiesStore.cpp
@@ -9,8 +9,72 @@ TEST_CASE("Testcases for Modifies Store") {
     modifies_store->addStatementModifyingVariable("2", "a");
     modifies_store->addStatementModifyingVariable("2", "b");
 
-    REQUIRE(modifies_store->retrieveAllVariablesModifiedByAStatement("2") == std::unordered_set<std::string>({ "a", "b" }));
+    REQUIRE(modifies_store->retrieveAllVariablesModifiedByAStatement("2") ==
+    std::unordered_set<std::string>({ "a", "b" }));
   }
+
+  SECTION("Empty Modifies store") {
+    ModifiesStore* modifies_store = new ModifiesStore();
+    REQUIRE(modifies_store->hasModifiesRelationBetweenStatementAndVariable("2", "a") == false);
+    REQUIRE(modifies_store->hasModifiesRelationBetweenProcedureAndVariable("anya", "a") == false);
+    REQUIRE(modifies_store->retrieveAllVariablesModifiedByAStatement("2") == std::unordered_set<PkbTypes
+    ::VARIABLE>({}));
+    REQUIRE(modifies_store->retrieveAllVariablesModifiedByAProcedure("anya") == std::unordered_set<PkbTypes::VARIABLE>({}));
+    REQUIRE(modifies_store->retrieveProcedureVariablePairs() == std::unordered_set<std::pair<PkbTypes::PROCEDURE, PkbTypes::VARIABLE>, PairHasherUtil::hash_pair>({}));
+    REQUIRE(modifies_store->retrieveStatementVariablePairs() == std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER,
+            PkbTypes::VARIABLE>, PairHasherUtil::hash_pair>({}));
+  }
+
+  SECTION("Single statement modifying a single variable") {
+    ModifiesStore* modifies_store = new ModifiesStore();
+
+    modifies_store->addStatementModifyingVariable("2", "a");
+    modifies_store->addStatementModifyingVariable("3", "b");
+    REQUIRE(modifies_store->retrieveAllVariablesModifiedByAStatement("2") == std::unordered_set<std::string>({"a"}));
+    REQUIRE_FALSE(modifies_store->retrieveAllVariablesModifiedByAStatement("3") == std::unordered_set<std::string>({"a"}));
+    REQUIRE(modifies_store->retrieveAllVariablesModifiedByAStatement("3") == std::unordered_set<std::string>({"b"}));
+    REQUIRE(modifies_store->retrieveStatementVariablePairs() ==
+    std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER, PkbTypes::VARIABLE>, PairHasherUtil::hash_pair>({
+      std::make_pair("2", "a"), std::make_pair("3", "b") }));
+    REQUIRE(modifies_store->hasModifiesRelationBetweenStatementAndVariable("2", "a") == true);
+    REQUIRE(modifies_store->hasModifiesRelationBetweenStatementAndVariable("3", "c") == false);
+  }
+
+  // This will result in error according to Sumanth - why?
+  SECTION("Single statement modifying multiple variables") {
+
+  }
+
+  SECTION("Multiple statements modifying a single variable") {
+
+  }
+
+
+  // should adhere to the rule that a single statement cannot modify multiple variables
+  SECTION("Multiple statements modifying a multiple variables") {
+
+  }
+
+
+  SECTION("Single Procedure modifying a single variable") {
+
+
+  }
+
+  SECTION("Single Procedure modifying multiple variables") {
+
+
+  }
+
+  SECTION("Multiple Procedures modifying single variable") {
+
+  }
+
+  SECTION("Multiple Procedures modifying multiple variables") {
+
+  }
+
+
 
 
 }
