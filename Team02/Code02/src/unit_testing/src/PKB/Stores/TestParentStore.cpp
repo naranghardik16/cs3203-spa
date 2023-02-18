7c28d07b7e0e1b875578399ee1ff033ae06d2078
Hardik Narang <naranghardik16@gmail.com>
2023-02-19 01:35:11 +0800

    :white_check_mark: add empty parent store unit test cases

--- Team02/Code02/src/unit_testing/src/PKB/Stores/TestParentStore.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestParentStore.cpp
@@ -2,6 +2,47 @@
 #include "PKB/Stores/ParentStore.h"
 
 TEST_CASE("Testcases for Parent Store") {
+
+  SECTION("Empty Parent Store") {
+    auto parent_store = new ParentStore();
+    REQUIRE(parent_store->hasAnyParentRelation() == false);
+    REQUIRE(parent_store->hasAnyParentStarRelation() == false);
+    REQUIRE(parent_store->hasParentStar("2") == false);
+    REQUIRE(parent_store->hasParentRelation("1", "2") == false);
+    REQUIRE(parent_store->hasParentStarBy("2") == false);
+    REQUIRE(parent_store->hasAnyParentRelation() == false);
+    REQUIRE(parent_store->retrieveAllParentPairs() == std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER,
+            PkbTypes::STATEMENT_NUMBER>, PairHasherUtil::hash_pair>({}));
+    REQUIRE(parent_store->retrieveAllParentStarPairs() == std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER,
+            PkbTypes::STATEMENT_NUMBER>, PairHasherUtil::hash_pair>({}));
+    REQUIRE(parent_store->hasParentStarRelation("1", "2") == false);
+    REQUIRE(parent_store->retrieveAllParents() == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({}));
+    REQUIRE(parent_store->retrieveAllParents("2") == PkbTypes::STATEMENT_NUMBER({}));
+    REQUIRE(parent_store->retrieveAllChildren() == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({}));
+    REQUIRE(parent_store->retrieveAllChildren("2") == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({}));
+    REQUIRE(parent_store->retrieveAllAncestors() == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({}));
+    REQUIRE(parent_store->retrieveAllAncestors("2") == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({}));
+    REQUIRE(parent_store->retrieveAllDescendants() == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({}));
+    REQUIRE(parent_store->retrieveAllDescendants("2") == std::unordered_set<PkbTypes::STATEMENT_NUMBER>({}));
+  }
+
+  SECTION("Independent Parent without any Transitive Relationship") {
+
+  }
+
+  SECTION("Dependent Parent with Single Transitive Relationship") {
+
+  }
+
+  SECTION("Dependednt Follows with Multiple Transitive Relationship") {
+
+  }
+
+  SECTION("Checking ParentStar") {
+
+
+  }
+
   SECTION("Basic Mapping") {
     auto* parent_store = new ParentStore();
 
