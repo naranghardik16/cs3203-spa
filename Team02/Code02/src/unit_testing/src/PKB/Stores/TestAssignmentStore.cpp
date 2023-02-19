07b09434e2041076cc23e9db6abcf2c84fcd5999
s7manth <syalamarty002@gmail.com>
2023-02-19 17:13:32 +0800

    remove dead files

--- Team02/Code02/src/unit_testing/src/PKB/Stores/TestAssignmentStore.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestAssignmentStore.cpp
@@ -3,12 +3,18 @@
 #include <string>
 #include <unordered_set>
 
-#include "PKB/Util/ExpressionUtil.h"
+#include "PKB/Stores/AssignmentStore.h"
 #include "SP/Parser/AssignStatementParser.h"
 
 TEST_CASE("Testcases for Assignment Store") {
   SECTION("Singleton (One Constant)") {
+    auto assignment_store = new AssignmentStore();
+    std::shared_ptr<Expression> root;
+    root = std::make_shared<Constant>("7");
 
+    assignment_store->addAssignmentExpression("1", root);
+
+//    REQUIRE(assignment_store->);
   }
 }
 
