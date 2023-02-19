bcd8c278149d204b9e84fdd63c245911c974c2af
s7manth <syalamarty002@gmail.com>
2023-02-19 16:10:05 +0800

    add skeleton for assignment store tests

---
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestAssignmentStore.cpp
@@ -0,0 +1,14 @@
+#include <catch.hpp>
+#include <memory>
+#include <string>
+#include <unordered_set>
+
+#include "PKB/Util/ExpressionUtil.h"
+#include "SP/Parser/AssignStatementParser.h"
+
+TEST_CASE("Testcases for Assignment Store") {
+  SECTION("Singleton (One Constant)") {
+
+  }
+}
+
