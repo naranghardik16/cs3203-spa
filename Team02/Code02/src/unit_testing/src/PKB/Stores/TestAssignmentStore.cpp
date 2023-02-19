6cf8542a0839036bad544693bbd0608045406658
s7manth <syalamarty002@gmail.com>
2023-02-19 19:23:22 +0800

    add test cases for assignment store

--- Team02/Code02/src/unit_testing/src/PKB/Stores/TestAssignmentStore.cpp
+++ Team02/Code02/src/unit_testing/src/PKB/Stores/TestAssignmentStore.cpp
@@ -14,7 +14,53 @@ TEST_CASE("Testcases for Assignment Store") {
 
     assignment_store->addAssignmentExpression("1", root);
 
-//    REQUIRE(assignment_store->);
+    REQUIRE(assignment_store->retrieveAssignmentExpressionByStatementNumber("1") == root);
+    REQUIRE(assignment_store->retrieveAllStatementNumbersWhichContainExpression(root) ==
+        std::unordered_set<PkbTypes::STATEMENT_NUMBER>({ "1" }));
+  }
+
+  SECTION("Singleton (One Variable)") {
+    auto assignment_store = new AssignmentStore();
+    std::shared_ptr<Expression> root;
+    root = std::make_shared<Variable>("x");
+
+    assignment_store->addAssignmentExpression("2", root);
+
+    REQUIRE(assignment_store->retrieveAssignmentExpressionByStatementNumber("2") == root);
+    REQUIRE(assignment_store->retrieveAllStatementNumbersWhichContainExpression(root) ==
+        std::unordered_set<PkbTypes::STATEMENT_NUMBER>({ "2" }));
+  }
+
+  SECTION("Simple Expression") {
+    auto assignment_store = new AssignmentStore();
+    std::pair<std::shared_ptr<Expression>, std::shared_ptr<Expression>> arguments;
+    arguments.first = std::make_shared<Constant>("11");
+    arguments.second = std::make_shared<Variable>("x");
+
+    auto root = std::make_shared<ArithmeticOperation>("+", arguments);
+
+    assignment_store->addAssignmentExpression("2", root);
+
+    REQUIRE(assignment_store->retrieveAssignmentExpressionByStatementNumber("2") == root);
+    REQUIRE(assignment_store->retrieveAllStatementNumbersWhichContainExpression(root) ==
+        std::unordered_set<PkbTypes::STATEMENT_NUMBER>({ "2" }));
+  }
+  
+  SECTION("Multiple Assignment Statements with the same RHS") {
+    auto assignment_store = new AssignmentStore();
+    std::pair<std::shared_ptr<Expression>, std::shared_ptr<Expression>> arguments;
+    arguments.first = std::make_shared<Constant>("11");
+    arguments.second = std::make_shared<Variable>("x");
+
+    auto root = std::make_shared<ArithmeticOperation>("+", arguments);
+
+    assignment_store->addAssignmentExpression("2", root);
+    assignment_store->addAssignmentExpression("3", root);
+
+    REQUIRE(assignment_store->retrieveAssignmentExpressionByStatementNumber("2") == root);
+    REQUIRE(assignment_store->retrieveAssignmentExpressionByStatementNumber("3") == root);
+    REQUIRE(assignment_store->retrieveAllStatementNumbersWhichContainExpression(root) ==
+        std::unordered_set<PkbTypes::STATEMENT_NUMBER>({ "2", "3" }));
   }
 }
 
