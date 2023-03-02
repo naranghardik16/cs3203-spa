da2e79640c890e9fdd8e9a0383fc4fac99efdae7
isabelteo <83572953+isabelteo@users.noreply.github.com>
2023-03-02 09:36:09 +0800

    Remove unused functions

--- Team02/Code02/src/spa/src/QPS/Clause/WithClauseSyntax.h
+++ Team02/Code02/src/spa/src/QPS/Clause/WithClauseSyntax.h
@@ -7,6 +7,4 @@ class WithClauseSyntax : public ClauseSyntax {
   bool Equals(ClauseSyntax &other) override;
   bool IsBooleanClause(Map &declaration_map) override;
   std::shared_ptr<ClauseEvaluator> CreateClauseEvaluator(Map &declaration_map) override;
-  bool IsAttrCondContainFalseAttrRefComparison(string attrRef, string comparison_value);
-  bool IsAttrCondContainMismatchedAttrRef(string attrRef_1, string attrRef_2);
 };
