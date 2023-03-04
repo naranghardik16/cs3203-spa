c8661fd1c2a59f7e05bbd1c425ddab9020ceb5e9
s7manth <syalamarty002@gmail.com>
2023-03-04 09:28:42 +0800

    add method implementations for calls store

--- Team02/Code02/src/spa/src/PKB/Stores/CallsStore.cpp
+++ Team02/Code02/src/spa/src/PKB/Stores/CallsStore.cpp
@@ -4,4 +4,55 @@ CallsStore::CallsStore() = default;
 
 CallsStore::~CallsStore() = default;
 
+void CallsStore::addCallsRelation(PkbTypes::PROCEDURE caller_procedure, PkbTypes::PROCEDURE callee_procedure) {
+  this->calls_store_.insert(caller_procedure, callee_procedure);
+  this->calls_star_store_.insert(caller_procedure, callee_procedure);
+
+  std::unordered_set<PkbTypes::PROCEDURE> callers =
+      this->calls_star_store_.retrieveFromValue(caller_procedure);
+
+  while (!callers.empty()) {
+    std::unordered_set<PkbTypes::PROCEDURE> updated_callers;
+    for (const auto& p: callers) {
+      std::unordered_set<PkbTypes::PROCEDURE> temp = this->calls_star_store_.retrieveFromValue(p);
+      updated_callers.insert(temp.begin(), temp.end());
+      this->calls_star_store_.insert(p, callee_procedure);
+    }
+    callers = updated_callers;
+  }
+}
+
+std::unordered_set<std::pair<PkbTypes::PROCEDURE, PkbTypes::PROCEDURE>,
+                   PairHasherUtil::hash_pair> CallsStore::retrieveAllCallsPairs() {
+  return this->calls_store_.retrieveAll();
+}
+
+std::unordered_set<std::pair<PkbTypes::PROCEDURE, PkbTypes::PROCEDURE>,
+                   PairHasherUtil::hash_pair> CallsStore::retrieveAllCallsStarPairs() {
+  return this->calls_star_store_.retrieveAll();
+}
+
+bool CallsStore::hasCallsRelation(PkbTypes::PROCEDURE caller_procedure, PkbTypes::PROCEDURE callee_procedure) {
+  return this->calls_store_.contains(caller_procedure, callee_procedure);
+}
+
+bool CallsStore::hasCallsStarRelation(PkbTypes::PROCEDURE caller_procedure, PkbTypes::PROCEDURE callee_procedure) {
+  return this->calls_star_store_.contains(caller_procedure, callee_procedure);
+}
+
+bool CallsStore::hasAnyCallsRelation() {
+  return this->calls_store_.length() > 0;
+}
+
+bool CallsStore::hasAnyCallsStarRelation() {
+  return this->calls_star_store_.length() > 0;
+}
+
+bool CallsStore::hasCallsStar(PkbTypes::PROCEDURE procedure) {
+  return !this->calls_star_store_.retrieveFromValue(procedure).empty();
+}
+
+bool CallsStore::hasCallsStarBy(PkbTypes::PROCEDURE procedure) {
+  return !this->calls_star_store_.retrieveFromKey(procedure).empty();
+}
 
