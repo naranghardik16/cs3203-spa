b47594db504d4e7f0af52e6cb502ebbdccc4b809
s7manth <syalamarty002@gmail.com>
2023-03-12 20:26:36 +0800

    add documentation for cfg store

--- Team02/Code02/src/spa/src/PKB/Stores/CFGStore.h
+++ Team02/Code02/src/spa/src/PKB/Stores/CFGStore.h
@@ -4,16 +4,47 @@
 #include "core/cfg_model/Cfg.h"
 #include "PKB/Types/PkbTypes.h"
 
+/**
+ * @class CFGStore
+ * Class representing the CFG Store in PKB.
+ *
+ * The CFGStore class is responsible for maintaining information about the
+ * control flow graph of the source program.
+ *
+ */
 class CFGStore {
  public:
+  /**
+   * Constructor for the CFGStore class.
+   */
   CFGStore();
 
+  /**
+   * Destructor for the CFGStore class.
+   */
   ~CFGStore();
 
+  /**
+   * Adds the control flow graph to the store.
+   *
+   * @param cfg - The CFG to be added.
+   */
   void addCfg(std::shared_ptr<Cfg> cfg);
 
+  /**
+   * Retrieves the CFG given a procedure.
+   *
+   * @param procedure - The procedure whose CFG is to be retrieved.
+   * @return The root node of the CFG for the given procedure.
+   */
   std::shared_ptr<CfgNode> getCfgNodeFromProcedure(PkbTypes::PROCEDURE procedure);
 
+  /**
+   * Retrieves the CFG node given a statement number.
+   *
+   * @param statement_number - The statement number representing the statement whose CFG node is to be retrieved.
+   * @return The CFG node for the given statement number.
+   */
   std::shared_ptr<CfgNode> getCfgNodeFromStatementNumber(PkbTypes::STATEMENT_NUMBER statement_number);
 
  private:
