c333e415b90e9af1c868978f9a04042930a5b183
Hardik Narang <naranghardik16@gmail.com>
2023-03-14 07:05:04 +0800

    :memo: add docs for NextStore

--- Team02/Code02/src/spa/src/PKB/Stores/NextStore.h
+++ Team02/Code02/src/spa/src/PKB/Stores/NextStore.h
@@ -5,29 +5,77 @@
 #include "CFGStore.h"
 #include "PKB/AbstractDataModels/ManyToManyStore.h"
 
+/**
+ * @class NextStore
+ * Class representing the Next Store in the PKB.
+ */
 class NextStore {
  public:
+  /**
+   * Constructor for Next store.
+   */
   NextStore();
 
+  /**
+   * Destructor for Next store.
+   */
   ~NextStore();
 
+  /**
+   * Sets the mapping of procedure name to its corresponding control flow graph root node in the PKB.
+   *
+   * @param ptonode - The mapping of procedure name to its corresponding control flow graph root node.
+   */
   void setProcedureToCfgRootNodeMap(std::unordered_map<PkbTypes::PROCEDURE, std::shared_ptr<CfgNode>> ptonode);
 
+  /**
+   * Sets the mapping of statement number to its corresponding control flow graph root node in the PKB.
+   *
+   * @param stonode - The mapping of statement number to its corresponding control flow graph root node.
+   */
   void setStatementNumberToCfgRootNodeMap(std::unordered_map<PkbTypes::STATEMENT_NUMBER,
                                           std::shared_ptr<CfgNode>> stonode);
 
+  /**
+   * Extracts all the Next relations between the statements in the control flow graph.
+   */
   void extractNextRelations();
 
+  /**
+   * Returns true if there exists a Next relation between the given statement numbers.
+   *
+   * @param statement_number - The statement number of the statement in the PKB.
+   * @param next_statement_number - The statement number of the next statement in the PKB.
+   * @return True if there exists a Next relation between the given statement numbers.
+   */
   bool hasNextRelation(PkbTypes::STATEMENT_NUMBER statement_number,
                        PkbTypes::STATEMENT_NUMBER next_statement_number);
 
+  /**
+   * Returns true if there exists any Next relation in the PKB.
+   * @return True if there exists any Next relation in the PKB.
+   */
   bool hasAnyNextRelation();
 
+  /**
+   *
+   * @return
+   */
   std::unordered_set<std::pair<PkbTypes::STATEMENT_NUMBER, PkbTypes::STATEMENT_NUMBER>, PairHasherUtil::hash_pair>
   retrieveAllNextPairs();
 
+  /**
+   * Returns all the pairs of statement numbers that have a Next relation between them in the PKB.
+   * @param statement_number
+   * @return A set of all pairs of statement numbers that have a Next relation between them in the PKB.
+   */
   bool hasNext(PkbTypes::STATEMENT_NUMBER statement_number);
 
+  /**
+   * Returns true if there exists any statement number that has a Next relation from the given statement number.
+   * @param statement_number - The statement number of the statement in the PKB.
+   * @return True if there exists any statement number that has a Next relation from the given statement number.
+   */
   bool hasNextBy(PkbTypes::STATEMENT_NUMBER statement_number);
 
 //  bool hasNextStarRelation(PkbTypes::STATEMENT_NUMBER statement_number, PkbTypes::STATEMENT_NUMBER next_statement_number);
@@ -38,8 +86,13 @@ class NextStore {
 //  retrieveAllNextStarPairs();
 
  private:
+  // A many-to-many store for PkbTypes::STATEMENT_NUMBER pairs representing Next relations between statements.
   ManyToManyStore<PkbTypes::STATEMENT_NUMBER, PkbTypes::STATEMENT_NUMBER> next_store_;
+
+  // A map from statement numbers to their corresponding CfgNode objects.
   std::unordered_map<PkbTypes::STATEMENT_NUMBER, std::shared_ptr<CfgNode>> statement_number_to_cfg_node_map_;
+
+  // A map from procedure names to their corresponding CfgNode objects.
   std::unordered_map<PkbTypes::PROCEDURE, std::shared_ptr<CfgNode>> procedure_name_to_cfg_node_map_;
 };
 
